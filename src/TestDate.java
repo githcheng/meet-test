69fba79fdb16905b16025e122bc80179ac3920cc
githcheng <chengguojiang2007@163.com>
2015-01-07 20:26:35 +0800

    add: TestDate

---
+++ src/TestDate.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * Created by guojiang on 2015/1/7.
+ */
+public class TestDate {
+
+    public static void main(String[] args){
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
